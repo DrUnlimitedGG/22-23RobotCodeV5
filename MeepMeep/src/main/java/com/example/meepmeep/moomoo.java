fb9203c34b71a29742b2677aa474521b3067c4aa
adityasaran <epicplayzquestions@gmail.com>
2022-11-21 15:24:14 -0400

    MeepMeep

--- MeepMeep/src/main/java/com/example/meepmeep/moomoo.java
+++ MeepMeep/src/main/java/com/example/meepmeep/moomoo.java
@@ -13,15 +13,8 @@ public class moomoo {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                        drive.trajectorySequenceBuilder(new Pose2d(-34, 60, -180))
                                 .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
                                 .build()
                 );
 
